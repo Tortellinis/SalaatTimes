d21636a1309daf0f72851b0a90bc1c5787ea47b2
Tortellinis <elhalihiba012006@gmail.com>
2025-02-02 12:05:27 +0100

    added mosques

--- js/mosques.js
+++ js/mosques.js
@@ -197,4 +197,286 @@ window.mosques = [
         lon: 11.0402091,
         address: "38068 Rovereto, Autonomous Province of Trento, Italy"
     },
+    {
+        name: "Centro culturale islamico Silà",
+        lat: 46.31339604988159,
+        lon: 11.272850781543452,
+        address: "Largo Municipio, 39044 Egna BZ, Italy"
+    },
+    {
+        name: "Mosque/“Il Dialogo” Association",
+        lat: 46.47305537098807,
+        lon: 11.334981096898026,
+        address: "Via Luigi Negrelli, 39100 Bolzano BZ"
+    },
+    {
+        name: "Muslim Cultural Center • Masjid",
+        lat: 46.49472628502996,
+        lon: 11.366664896899248,
+        address: "Via del Macello, 18, 39100 Bolzano BZ"
+    },
+    {
+        name: "Centro Culturale Islamico Al-Salam",
+        lat: 46.477927182747834,
+        lon: 11.337874868061986,
+        address: "Via Alessandro Volta, 1, 39100 Bolzano BZ"
+    },
+    {
+        name: "Bangladeshi Mosque",
+        lat: 46.49117967098887, 
+        lon: 11.341131268062814,
+        address: "Via Torino, 13g, 39100 Bolzano BZ"
+    },
+    {
+        name: "Moschea 'Dialogo e Pace'",
+        lat: 46.63621333755748,
+        lon: 11.175554196907532,
+        address: "Via Enrico Fermi, 1/E, 39012 Sinigo BZ"
+    },
+    {
+        name: "'Omar Bin Hattab' Masjid",
+        lat: 46.66511314542961, 
+        lon: 11.153533525745543,
+        address: "Via Monte Tessa, 22/A, 39012 Merano BZ"
+    },
+    {
+        name: "Moschea di Silandro",
+        lat: 46.621340002251436,
+        lon: 10.814316568070348,
+        address: "Zona Produttiva, 27m, 39028 Vezzano BZ"
+    },
+    {
+        name: "Association Al Amana Cultural Center Mosque / Masjid Anour Milland / Bressanone",
+        lat: 46.70780678617075,
+        lon: 11.663136427602293,
+        address: "Via Vintler, 5, 39042 Bressanone BZ"
+    },
+    {
+        name: "Faizan e Madina Fortezza",
+        lat: 46.78719052486465,
+        lon: 11.611978839243767,
+        address: "39045 Franzensfeste, Autonomous Province of Bolzano – South Tyrol"
+    },
+    {
+        name: "Brunico's Masjid(Mosque)",
+        lat: 46.78514677816635,
+        lon: 11.921669196916222,
+        address: "Via Johann Georg Mahl, 40, 39031 Brunico BZ"
+    },
+    {
+        name: "Associazione Islamica Baitul Falah",
+        lat: 45.57642434178001,
+        lon: 10.713378139173564,
+        address: "Via Marco Polo, 2, 37016 Garda VR"
+    },
+    {
+        name: "Moschea Al Noor Lonato del Garda",
+        lat: 45.467942422513765,
+        lon: 10.470779096839998,
+        address: "Fenil Nuovo Molini ang., Via Colombere Pozze, 25017 Lonato BS"
+    },
+    {
+        name: "Moschea di Brescia - Centro Culturale Islamico di Brescia (BS)",
+        lat: 45.520655665649834,
+        lon: 10.193517013533837,
+        address: "Via Corsica, 361, 25125 Brescia BS"
+    },
+    {
+        name: "Moschea",
+        lat: 45.54675503028413,
+        lon: 9.698352844871348,
+        address: "Viale Vittorio Veneto, 161, 24047 Treviglio BG"
+    },
+    {
+        name: "Omar Al Farouk Mosque",
+        lat: 45.827244241888586,
+        lon: 8.930511505151207,
+        address: "Via Giuseppe Giusti, 14, 21100 Varese VA"
+    },
+    {
+        name: "Moschea Assabil 'La Retta Via'",
+        lat: 45.39710863869845,
+        lon: 9.292180521115663,
+        address: "Via Marzabotto, 8/10, 20098 San Giuliano Milanese MI"
+    },
+    {
+        name: "Islamic Community Fajr-Comunita Islamica Fajr",
+        lat: 45.43700083393795,
+        lon: 9.208908370326446,
+        address: "Via Bernardo Quaranta, 54, 20139 Milano MI"
+    },
+    {
+        name: "Moschea zona Lorenteggio/Giambellino",
+        lat: 45.44203787095466,
+        lon: 9.120822090102026,
+        address: "Via Francesco Gonin, 56, 20147 Milano MI"
+    },
+    {
+        name: "Centro Islamico Dar al Quran",
+        lat: 45.434469559995954,
+        lon: 9.182175197361438,
+        address: "Via Stadera, 18, 20141 Milano MI"
+    },
+    {
+        name: "Moschea Mariam",
+        lat: 45.51260243852949,
+        lon: 9.257054333909386,
+        address: "Via Padova, 366, 20132 Milano MI"
+    },
+    {
+        name: "Casa della Cultura Islamica Onlus",
+        lat: 45.498285856385124,
+        lon: 9.232249265795794,
+        address: "Via Padova, 144, 20132 Milano MI"
+    },
+    {
+        name: "Associazione Culturale Al Nur Italia",
+        lat: 45.491321748865396,
+        lon: 9.200985435226746,
+        address: "Via Giacomo Carissimi, 19, 20124 Milano MI"
+    },
+    {
+        name: "Masjid - mosque - Moschea - مسجد",
+        lat: 45.49601471942084,
+        lon: 9.193861487438445,
+        address: "Viale Marche, 40, 20159 Milano MI"
+    },
+    {
+        name: "Moschea Milano Sesto",
+        lat: 45.55093248934741,
+        lon: 9.24902916432544,
+        address: "Via Bernardino Luini, 1-408, 20099 Sesto San Giovanni MI"
+    },
+    {
+        name: "Islamic Center of Monza and Brianza",
+        lat: 45.57545150522326,
+        lon: 9.275810703604542,
+        address: "Via Ghilini, 17, 20900 Monza MB"
+    },
+    {
+        name: "Moschea Pavia",
+        lat: 45.18568561532161,
+        lon: 9.169859039460555,
+        address: "Via San Giovannino, 4/A, 27100 Pavia PV"
+    },
+    {
+        name: "Moschea di Gardolo",
+        lat: 46.107201194286326,
+        lon: 11.10871848510059,
+        address: "Via Soprasasso, 22, 38121 Trento TN"
+    },
+    {
+        name: "Moschea Camii",
+        lat: 45.7091968042924,
+        lon: 11.357332830390718,
+        address: "Via Venezia, 62, 36015 Schio VI"
+    },
+    {
+        name: "Centro Culturale Islamico Ettawba - Moschea di Vicenza",
+        lat: 45.53696007811308,
+        lon: 11.503891200048356,
+        address: "Via Vecchia Ferriera, 167, 36100 Vicenza VI"
+    },
+    {
+        name: "Moschea centrale di Vicenza",
+        lat: 45.54453187284414,
+        lon: 11.532132178766677,
+        address: "Via dei Mille, 6, 36100 Vicenza VI"
+    },
+    {
+        name: "Moschea Al-Farouk",
+        lat: 45.4225459475003,
+        lon: 11.871018300081502,
+        address: "Corso Tre Venezie, 20, 35135 Padova PD"
+    },
+    {
+        name: "Moschea assalam",
+        lat: 45.68316740183272,
+        lon: 11.973831654039431,
+        address: "Via per San Floriano, 58/c, 31033 Castelfranco Veneto TV"
+    },
+    {
+        name: "Moschea di Bologna (BO) - An-Nur 'La Luce'",
+        lat: 44.49319922527598,
+        lon: 11.35630130945178,
+        address: "Via Torleone, 34/B, 40125 Bologna BO"
+    },
+    {
+        name: "Moschea An-Nur 'La Luce'",
+        lat: 44.49049948249413,
+        lon: 11.402387023416578,
+        address: "Via Pallavicini, 13, 40138 Bologna BO"
+    },
+    {
+        name: "Moschea - Ciserano",
+        lat: 45.59337026408057,
+        lon: 9.605181639409244,
+        address: "Corso Europa, 51, 24040 Ciserano BG"
+    },
+    {
+        name: "Moschea Annur (Centro Culturale Islamico)",
+        lat: 45.86536962754815,
+        lon: 10.142745226843838,
+        address: "Via S. Caterina da Siena, 9, 24060 Rogno BG"
+    },
+    {
+        name: "Centro Culturale Islamico di Bergamo",
+        lat: 45.696283097566024,
+        lon: 9.69212082548196,
+        address: "Via Monte Cenisio, 3, 24121 Bergamo BG"
+    },
+    {
+        name: "Moschea di San Paolo d'Argon",
+        lat: 45.68113422716607,
+        lon: 9.789652526823092,
+        address: "Via A. Volta, 19, 24060 San Paolo d'Argon BG"
+    },
+    {
+        name: "Moschea di Montello",
+        lat: 45.672095543631016,
+        lon: 9.80686157986158,
+        address: "Via IV Novembre, 24060 Montello BG"
+    },
+    {
+        name: "Centro Islamico Assalam Lecco مسجد السلام",
+        lat: 45.822268771371725,
+        lon: 9.418473821012025,
+        address: "Corso Bergamo, 92, 23900 Lecco LC"
+    },
+    {
+        name: "Lecco Mosque",
+        lat: 45.84485984634087,
+        lon: 9.399624467118276,
+        address: "Corso Carlo Alberto, 16, 23900 Lecco LC"
+    },
+    {
+        name: "Centro Culturale Islamico di Gera Lario",
+        lat: 46.14510738412683,
+        lon: 9.410642981751444,
+        address: "Via Trivio di Fuentes, 11, Gera Lario CO"
+    },
+    {
+        name: "Moschea turca",
+        lat: 45.78924889734995,
+        lon: 9.088468104823129,
+        address: "Via Filippo Turati, 5, 22100 Como CO"
+    },
+    {
+        name: "Centro culturale islamico di Mozzate",
+        lat: 45.67701189993161,
+        lon: 8.945449090904786,
+        address: "Via Ezio Tarantelli, 16b, 22076 Mozzate CO"
+    },
+    {
+        name: "Centro Culturale Islamico Tradate",
+        lat: 45.70742674850772,
+        lon: 8.908620625956017,
+        address: "Via Chiesa Damiano, 21, 21049 Tradate VA"
+    },
+    {
+        name: "Centro Culturale Islamico & moschea",
+        lat: 45.71826852377113,
+        lon: 8.27880807154382,
+        address: "Via Partigiani, 14, 13011 Borgosesia VC"
+    },
 ];
